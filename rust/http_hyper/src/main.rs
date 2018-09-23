864a8c9382a6d4c62a94578b102065af00e390a6
Marek Siarkowicz <serathius@users.noreply.github.com>
2018-09-23 17:17:36 +0200

    Fix rust

--- rust/http_hyper/src/main.rs
+++ rust/http_hyper/src/main.rs
@@ -1,37 +1,25 @@
 #![deny(warnings)]
 extern crate hyper;
-extern crate futures;
-extern crate pretty_env_logger;
 
-use futures::future::FutureResult;
+use hyper::{Body, Request, Response, Server};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
 
-use hyper::header::{ContentLength, ContentType};
-use hyper::server::{Http, Service, Request, Response};
+const PHRASE: &str = "Hello, World!";
 
-static PHRASE: &'static [u8] = b"Hello World!";
-
-struct Hello;
-
-impl Service for Hello {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = FutureResult<Response, hyper::Error>;
-    fn call(&self, _req: Request) -> Self::Future {
-        futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_header(ContentType::plaintext())
-                .with_body(PHRASE)
-        )
-    }
-
+fn hello_world(_req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from(PHRASE))
 }
 
+
 fn main() {
-    pretty_env_logger::init().unwrap();
-    let addr = "127.0.0.1:3000".parse().unwrap();
-    let server = Http::new().bind(&addr, || Ok(Hello)).unwrap();
-    println!("Listening on http://{} with 1 thread.", server.local_addr().unwrap());
-    server.run().unwrap();
+    let addr = ([127, 0, 0, 1], 8000).into();
+    let new_svc = || {
+        // service_fn_ok converts our function into a `Service`
+        service_fn_ok(hello_world)
+    };
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+    hyper::rt::run(server);
 }

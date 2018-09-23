591288571389b915102d6c57d08659449fd5cf18
Marek Siarkowicz <serathius@users.noreply.github.com>
2018-09-23 16:33:28 +0200

    Add other python servers

--- python/http_falcon.py
+++ python/http_falcon.py
@@ -1,5 +1,3 @@
-import json
-
 import falcon
 
 
@@ -10,3 +8,16 @@ class HelloResource:
 
 app = falcon.API()
 app.add_route('/', HelloResource())
+
+if os.environ.get("SERVER") == 'bjoern':
+    import bjoern
+    bjoern.run(app, '127.0.0.1', 8000)
+
+if os.environ.get("SERVER") == 'netius':
+    import netius.servers
+    server = netius.servers.WSGIServer(app = app)
+    server.serve(port = 8000)
+
+if os.environ.get("SERVER") == 'waitress':
+    from waitress import serve
+    serve(app, listen='*:8000')
--- python/http_flask.py
+++ python/http_flask.py
@@ -6,3 +6,16 @@ app = flask.Flask("app")
 @app.route("/")
 def simple():
     return 'Hello world!'
+
+if os.environ.get("SERVER") == 'bjoern':
+    import bjoern
+    bjoern.run(app, '127.0.0.1', 8000)
+
+if os.environ.get("SERVER") == 'netius':
+    import netius.servers
+    server = netius.servers.WSGIServer(app = app)
+    server.serve(port = 8000)
+
+if os.environ.get("SERVER") == 'waitress':
+    from waitress import serve
+    serve(app, listen='*:8000')
